364243261f4b002d288c30b6b3e7cce6150f7b15
lambys-89 <75185120+lambys-89@users.noreply.github.com>
2021-02-06 14:06:44 +0000

    SheCodes Final Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Loader from "react-loader-spinner";
+import Search from "./search.js"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Loader 
+        type = "Grid"
+        color= "#00BFFF"
+        height={100}
+        width={100}
+        timeout={5000}
+        />
+        <h1>
+          Welcome to the Weather App
+        </h1>
+        <button>Current Location</button>
+        <Search />
       </header>
+      <footer className="App-footer">
+        <p className="open-source-code">
+          <a className="App-link" href="https://github.com/lambys-89/shecodes-plus-project">
+            Open-source code
+          </a>{" "}
+          by Sarah Lamb
+        </p>
+      </footer>
     </div>
   );
 }
